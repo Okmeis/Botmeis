575b6ad230e8276b688a9905babdf296da2e9898
Okmeis <m-rafael96@web.de>
2021-03-09 17:53:35 +0100

    Added Start-Button, Connection-Method and some TextBoxes to MainForm

--- Botmeis/MainForm.Designer.cs
+++ Botmeis/MainForm.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbChannel = new System.Windows.Forms.TextBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbOAuth = new System.Windows.Forms.TextBox();
+            this.tbStartMessage = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // backgroundWorker1
@@ -37,20 +42,69 @@
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
             this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
             // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(358, 160);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbChannel
+            // 
+            this.tbChannel.Location = new System.Drawing.Point(85, 93);
+            this.tbChannel.Name = "tbChannel";
+            this.tbChannel.Size = new System.Drawing.Size(100, 20);
+            this.tbChannel.TabIndex = 1;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(85, 119);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 20);
+            this.tbUsername.TabIndex = 2;
+            // 
+            // tbOAuth
+            // 
+            this.tbOAuth.Location = new System.Drawing.Point(85, 145);
+            this.tbOAuth.Name = "tbOAuth";
+            this.tbOAuth.Size = new System.Drawing.Size(100, 20);
+            this.tbOAuth.TabIndex = 3;
+            // 
+            // tbStartMessage
+            // 
+            this.tbStartMessage.Location = new System.Drawing.Point(85, 171);
+            this.tbStartMessage.Name = "tbStartMessage";
+            this.tbStartMessage.Size = new System.Drawing.Size(100, 20);
+            this.tbStartMessage.TabIndex = 4;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbStartMessage);
+            this.Controls.Add(this.tbOAuth);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.tbChannel);
+            this.Controls.Add(this.btnStart);
             this.Name = "MainForm";
             this.Text = "Botmeis";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbChannel;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbOAuth;
+        private System.Windows.Forms.TextBox tbStartMessage;
     }
 }
 
--- Botmeis/MainForm.cs
+++ Botmeis/MainForm.cs
@@ -1,12 +1,40 @@
-﻿using System.ComponentModel;
+﻿using Botmeis.Communication;
+using System;
+using System.ComponentModel;
 using System.Windows.Forms;
 
-namespace Botmeis
-{
+namespace Botmeis {
     public partial class MainForm : Form {
+
+        IrcClient irc;
+        String message;
+
         public MainForm() {
             InitializeComponent();
         }
+
+        private void btnStart_Click(object sender, EventArgs e) {
+
+            if (tbUsername.Text != "" && tbOAuth.Text != "" && tbChannel.Text != "") {
+                btnStart.Enabled = false;
+                ircConnect();
+                backgroundWorker1.RunWorkerAsync();
+                //implement later
+                //btnChange.Enabled = true;
+                tbUsername.Enabled = false;
+                tbOAuth.Enabled = false;
+                //implement later:
+                //btnLoadProfile.Enabled = false;
+            }
+        }
+
+        private void ircConnect() {
+            irc = new IrcClient("irc.twitch.tv", 6667, tbUsername.Text, tbOAuth.Text);
+            irc.joinRoom(tbChannel.Text.ToLower());
+            irc.sendChatMessage(tbStartMessage.Text);
+        }
+
+
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) {
 
@@ -15,5 +43,6 @@ namespace Botmeis
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
 
         }
+
     }
 }
